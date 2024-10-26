907b1cc2d0726a5950f9ae1412190ded556d9bcb
juli2kapo <sesto@elykia.com.ar>
2024-10-26 20:25:03 -0300

    .

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 const twilio = require('twilio')
 const Router = require('express').Router
+const express = require('express')
 const ivrRouter = require('./ivr/router')
 
 const router = new Router()
@@ -7,6 +8,8 @@ const router = new Router()
 router.get('/', async (req, res) => {
   res.send('Elykia API')
 })
+
+router.use('/public', express.static('public'))
 
 router.use('/ivr', twilio.webhook({ validate: false }), ivrRouter)
 
