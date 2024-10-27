038b60d23561b6501c7a02b82c8f5bb6866b5702
juli2kapo <sesto@elykia.com.ar>
2024-10-26 21:18:40 -0300

    removed log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 const config = require('./config');
-console.log("AAAAAA");
 const server = app.listen(config.port, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
