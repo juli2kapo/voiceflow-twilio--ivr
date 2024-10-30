b04f9302fd51762b615f38f98ad8767a5e2f0a3c
juli2kapo <sesto@elykia.com.ar>
2024-10-30 09:08:20 -0300

    fixed audio files

--- src/menu/router.js
+++ src/menu/router.js
@@ -5,6 +5,24 @@ const fs = require("fs");
 
 const router = new Router();
 
+const createdMP3s = new Map([
+    // ['Hola buenos dias, bienvenido al restaurante Elykia, como puedo ayudarlo?', 'FirstWelcome.mp3'],
+    // ['¿Para qué día y hora quiere la reserva?', 'GetDayAndHour.mp3'],
+    // ['¿Para cuánta gente reservo la mesa?', 'GetPeople.mp3'],
+    // ['¿A qué hora quiere que sea la reserva?', 'GetHour.mp3'],
+    // ['¿Bajo qué nombre desea colocar la reserva?', 'GetName.mp3'],
+    // ['Entiendo tu situación, ahora mismo te podríamos redirigir a personal competente del restaunte si esta no fuese una demo. ¿Te puedo ayudar en algo más?', 'NotReclamo.mp3'],
+    // ['Esto es una demo y en Elykia no hacemos comida, pero si lo haríamos podrías pedir ahora mismo. ¿En qué más puedo ayudarte?', 'NotDelivery.mp3'],
+    // ['¿Hay algo más con lo que pueda ayudarte?', 'QuestionVariation1.mp3'],
+    // ['Disculpame, para buscar tu turno necesito que me digas el nombre bajo el que esta la reserva', 'MissingName.mp3'],
+    // ['Disculpa, no encontré ninguna reserva bajo ese nombre, ¿podría ser que este bajo otro nombre?', 'NoReserva.mp3'],
+    // ['¿Puedo ayudarlo con algo mas?', 'QuestionVariation2.mp3'],
+    // ['Ahora mismo no contamos con mesas para esa cantidad de gente, al ser una demo son mesas simuladas y no podemos juntarlas ni hacer más ¿Puedo ayudarte con algo mas?', 'NoMesas.mp3'],
+    // ['Cuando querria agendar el nuevo turno y para cuantas personas seria?', 'GetDayHourAndPeople.mp3'],
+    // ['Muchas gracias por comunicarte con Elykia, que tengas un lindo dia', 'Despedida.mp3'],
+  ]);
+
+
 const workHours = [
     "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"
 ];
@@ -193,46 +211,49 @@ router.post('/checkName', (req, res) => {
     });
 });
 
-// router.get("/createMP3",(req,res)=>{
-//     async function getMP3(textToMake) {
-//         const options = {
-//           method: 'POST',
-//           headers: {
-//             'xi-api-key': process.env.ELEVEN_LABS_API_KEY,
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify({
-//             text: textToMake,
-//             language_code: "es",
-//             voice_settings: { stability: 0.45, similarity_boost: 1 },
-//             model_id: "eleven_turbo_v2_5",
-//           }),
-//         };
+router.get("/createMP3",(req,res)=>{
+    async function saveMP3(textToMake,outputName) {
+        const options = {
+          method: 'POST',
+          headers: {
+            'xi-api-key': process.env.ELEVEN_LABS_API_KEY,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            text: textToMake,
+            language_code: "es",
+            voice_settings: { stability: 0.45, similarity_boost: 1 },
+            model_id: "eleven_turbo_v2_5",
+          }),
+        };
     
-//         try {
-//           const response = await fetch('https://api.elevenlabs.io/v1/text-to-speech/2Lb1en5ujrODDIqmp7F3', options);
+        try {
+          const response = await fetch('https://api.elevenlabs.io/v1/text-to-speech/2Lb1en5ujrODDIqmp7F3', options);
     
-//           if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//           }
+          if (!response.ok) {
+            // const error = await response.text();
+            // throw new Error(`HTTP error! status: ${response.status} code: ${error}`);
+            throw new Error(`Error en: ${filePath}`);
+          }
     
-//           const arrayBuffer = await response.arrayBuffer(); // Await the ArrayBuffer conversion
-//           const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
-//         //   const filePath = '/home/ubuntu/twilioivr/voiceflow-twilio--ivr/public/output.mp3';
-//           const filePath = "output.mp3"
+          const arrayBuffer = await response.arrayBuffer(); // Await the ArrayBuffer conversion
+          const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
+        //   const filePath = '/home/ubuntu/twilioivr/voiceflow-twilio--ivr/public/output.mp3';
+          const filePath = outputName
     
-//           fs.writeFileSync(filePath, buffer); // Save the buffer as an MP3 file
-//           console.log(`Audio saved to ${filePath}`);
+          fs.writeFileSync(filePath, buffer); // Save the buffer as an MP3 file
+          console.log(`Audio saved to ${filePath}`);
     
-//         } catch (err) {
-//           console.error('Error:', err); // Catch and log any errors
-//         }
-//       }
+        } catch (err) {
+          console.error('Error:', err); // Catch and log any errors
+        }
+      }
 
 
-//     const text = "Muchas gracias por comunicarte con Elykia, que tengas un lindo dia";
-//     getMP3(text);
-//     return res.json("MP3 created")
-// })
+    createdMP3s.forEach(async (value,key)=>{
+        await saveMP3(key,value);
+    })
+    return res.json("MP3 created")
+})
 
 module.exports = router;
