7518693408968c9da475604d5a25c14c76d6b7ce
brezza <brezza@dc.uba.ar>
2024-11-02 18:02:27 -0300

    .

--- src/menu/router.js
+++ src/menu/router.js
@@ -123,7 +123,7 @@ router.post('/deleteTurns', (req, res) => {
 router.post('/createTurns', (req, res) => {
     const { startDate, endDate, amountOfPeople, responsibleName } = req.body;
     console.log("message.toolCalls",req.body.message.toolCalls)
-    console.log("message.toolCalls.function",req.body.message.toolCalls.function)
+    console.log("message.toolCalls.function",req.body.message.toolCalls[0].function)
     const fechaInicio = new Date(startDate);
     const fechaFin = new Date(endDate);
     const tablesQuery = `SELECT * FROM tables WHERE seats >= ?`;
